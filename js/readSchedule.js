436fea498ffccea140a79ca4d424d4563ff24248
Nikita <40892020+mrklimka69@users.noreply.github.com>
2025-04-02 17:46:10 +0300

    Add files via upload

--- js/readSchedule.js
+++ js/readSchedule.js
@@ -5,48 +5,81 @@ function read() {
     let fullText = document.getElementById('fullText_input').value + "\n";
     let rows = getOnlyNeededRows(getRows(fullText));
 
-    console.log(getJobTitle("Пн(31.03) > 11:30 - 20:00 (08:00) инициатор"));
-
     for (let i = 0; i < rows.length; i++) {
         let dayOfTheWeek = getDay(rows[i]);
 
-        
+        switch (dayOfTheWeek) {
+            case "пн": {
+                document.getElementById("row_time_start_pn").value = getStartTime(rows[i]);
+                document.getElementById("row_time_finish_pn").value = getFinishTime(rows[i]);
+                document.getElementById("row_input_jobName_pn").value = getJobTitleOnly(rows[i]);
 
-        // switch (dayOfTheWeek) {
-        //     case "пн": {
-        //         document.getElementById("row_time_start_pn").value = getStartTime(rows[i]);
-        //         document.getElementById("row_time_finish_pn").value = getFinishTime(rows[i]);
-        //         document.getElementById("row_input_jobName_pn").value = getJobTitle(rows[i]);
-                
+                document.getElementById("choose_wrapper_top_checkbox_pn").checked = isBreak(rows[i]);
+                document.getElementById("choose_wrapper_bottom_checkbox_pn").checked = isDoublePunch(rows[i]);
 
+                continue;
+            }
+            case "вт": {
+                document.getElementById("row_time_start_vt").value = getStartTime(rows[i]);
+                document.getElementById("row_time_finish_vt").value = getFinishTime(rows[i]);
+                document.getElementById("row_input_jobName_vt").value = getJobTitleOnly(rows[i]);
 
-        //         continue;
-        //     }
-        //     case "вт": {
-        //         console.log("Это вторник");
-        //         continue;
-        //     }
-        //     case "ср": {
-        //         console.log("Это среда");
-        //         continue;
-        //     }
-        //     case "чт": {
-        //         console.log("Это четверг");
-        //         continue;
-        //     }
-        //     case "пт": {
-        //         console.log("Это пятница");
-        //         continue;
-        //     }
-        //     case "сб": {
-        //         console.log("Это суббота");
-        //         continue;
-        //     }
-        //     case "вс": {
-        //         console.log("Это воскресенье");
-        //         continue;
-        //     }
-        // }
+                document.getElementById("choose_wrapper_top_checkbox_vt").checked = isBreak(rows[i]);
+                document.getElementById("choose_wrapper_bottom_checkbox_vt").checked = isDoublePunch(rows[i]);
+
+                continue;
+            }
+            case "ср": {
+                document.getElementById("row_time_start_sr").value = getStartTime(rows[i]);
+                document.getElementById("row_time_finish_sr").value = getFinishTime(rows[i]);
+                document.getElementById("row_input_jobName_sr").value = getJobTitleOnly(rows[i]);
+
+                document.getElementById("choose_wrapper_top_checkbox_sr").checked = isBreak(rows[i]);
+                document.getElementById("choose_wrapper_bottom_checkbox_sr").checked = isDoublePunch(rows[i]);
+
+                continue;
+            }
+            case "чт": {
+                document.getElementById("row_time_start_cht").value = getStartTime(rows[i]);
+                document.getElementById("row_time_finish_cht").value = getFinishTime(rows[i]);
+                document.getElementById("row_input_jobName_cht").value = getJobTitleOnly(rows[i]);
+
+                document.getElementById("choose_wrapper_top_checkbox_cht").checked = isBreak(rows[i]);
+                document.getElementById("choose_wrapper_bottom_checkbox_cht").checked = isDoublePunch(rows[i]);
+
+                continue;
+            }
+            case "пт": {
+                document.getElementById("row_time_start_pt").value = getStartTime(rows[i]);
+                document.getElementById("row_time_finish_pt").value = getFinishTime(rows[i]);
+                document.getElementById("row_input_jobName_pt").value = getJobTitleOnly(rows[i]);
+
+                document.getElementById("choose_wrapper_top_checkbox_pt").checked = isBreak(rows[i]);
+                document.getElementById("choose_wrapper_bottom_checkbox_pt").checked = isDoublePunch(rows[i]);
+
+                continue;
+            }
+            case "сб": {
+                document.getElementById("row_time_start_sb").value = getStartTime(rows[i]);
+                document.getElementById("row_time_finish_sb").value = getFinishTime(rows[i]);
+                document.getElementById("row_input_jobName_sb").value = getJobTitleOnly(rows[i]);
+
+                document.getElementById("choose_wrapper_top_checkbox_sb").checked = isBreak(rows[i]);
+                document.getElementById("choose_wrapper_bottom_checkbox_sb").checked = isDoublePunch(rows[i]);
+
+                continue;
+            }
+            case "вс": {
+                document.getElementById("row_time_start_vs").value = getStartTime(rows[i]);
+                document.getElementById("row_time_finish_vs").value = getFinishTime(rows[i]);
+                document.getElementById("row_input_jobName_vs").value = getJobTitleOnly(rows[i]);
+
+                document.getElementById("choose_wrapper_top_checkbox_vs").checked = isBreak(rows[i]);
+                document.getElementById("choose_wrapper_bottom_checkbox_vs").checked = isDoublePunch(rows[i]);
+
+                continue;
+            }
+        }
     }
 }
 
@@ -99,11 +132,10 @@ function getFinishTime(row) {
     return row.substring(20, 25);
 }
 
-function getJobTitle(row) {
+function getJobTitleOnly(row) {
     let jobTitle = "";
     let iterator = row.length - 1;
     let counter = 0;
-    console.log("Вызов функции");
 
     while (row[iterator] != " ") {
         iterator--;
@@ -116,12 +148,29 @@ function getJobTitle(row) {
         iterator++;
     }
 
-    
-    console.log("Возвращаю - " + jobTitle);
-
     return jobTitle.trim();
 }
 
 function getDay(row) {
     return row.toLowerCase().substring(0, 2);
+}
+
+function isDoublePunch(row) {
+    row = row.slice(0, getJobTitleOnly(row).length * -1);
+
+    if (row.indexOf(' дп') == -1) {
+        return false
+    } else {
+        return true;
+    }
+}
+
+function isBreak(row) {
+    row = row.slice(0, getJobTitleOnly(row).length * -1);
+
+    if (row.indexOf(' б') == -1) {
+        return false
+    } else {
+        return true;
+    }
 }
